55c67dd930918b5ee983ecd645be5b2608503e15
mmjcap <90554849+mmjcap@users.noreply.github.com>
2025-01-10 23:24:22 +0100

    Update time_table_ita.cpp

--- time_table_ita.cpp
+++ time_table_ita.cpp
@@ -5,67 +5,92 @@ int main() {
 
     do {
         // Chiede all'utente di inserire il giorno della settimana da visualizzare
-        printf("\nInserisci il giorno della settimana (1=Lunedi', 2=Martedi', 3=Mercoledi', 4=Giovedi', 5=Venerdi', 0=Tutta Settimana): ");
+        printf("\nInserisci il giorno della settimana (1=Lunedi', 2=Martedi', 3=Mercoledi', 4=Giovedi', 5=Venerdi', 6=Sabato, 0=Tutta Settimana): ");
         scanf("%d", &giorno);
 
         // Visualizza l'orario scolastico del giorno selezionato
         switch (giorno) {
             case 1:
                 printf("\nLunedi':\n\n");
-                printf("1) italiano\n");
-                printf("2) storia\n");
-                printf("3) matematica\n");
-                printf("4) matematica\n");
-                printf("5) storia\n");
-                printf("6) scienze\n");
+                printf("1) -\n");
+                printf("2) -\n");
+                printf("3) -\n");
+                printf("4) -\n");
+                printf("5) -\n");
+                printf("6) -\n");
+                /*printf("7) -\n");
+                printf("8) -\n");*/
                 break;
                 
             case 2:
                 printf("\nMartedi':\n\n");
-                printf("1) fisica\n");
-                printf("2) fisica\n");
-                printf("3) arte\n");
-                printf("4) scienze\n");
-                printf("5) inglese\n");
-                printf("6) filosofia\n");
+                printf("1) -\n");
+                printf("2) -\n");
+                printf("3) -\n");
+                printf("4) -\n");
+                printf("5) -\n");
+                printf("6) -\n");
+                /*printf("7) -\n");
+                printf("8) -\n");*/
                 break;
                 
             case 3:
                 printf("\nMercoledi':\n\n");
-                printf("1) italiano\n");
-                printf("2) italiano\n");
-                printf("3) informatica\n");
-                printf("4) matematica\n");
-                printf("5) ginnastica\n");
-                printf("6) ginnastica\n");
+                printf("1) -\n");
+                printf("2) -\n");
+                printf("3) -\n");
+                printf("4) -\n");
+                printf("5) -\n");
+                printf("6) -\n");
+                /*printf("7) -\n");
+                printf("8) -\n");*/
                 break;
                 
             case 4:
                 printf("\nGiovedi':\n\n");
-                printf("1) scienze\n");
-                printf("2) inglese\n");
-                printf("3) filosofia\n");
-                printf("4) fisica\n");
-                printf("5) religione\n");
-                printf("6) arte\n");
+                printf("1) -\n");
+                printf("2) -\n");
+                printf("3) -\n");
+                printf("4) -\n");
+                printf("5) -\n");
+                printf("6) -\n");
+                /*printf("7) -\n");
+                printf("8) -\n");*/
                 break;
                 
             case 5:
                 printf("\nVenerdi':\n\n");
-                printf("1) informatica\n");
-                printf("2) inglese\n");
-                printf("3) scienze\n");
-                printf("4) scienze\n");
-                printf("5) italiano\n");
-                printf("6) matematica\n");
+                printf("1) -\n");
+                printf("2) -\n");
+                printf("3) -\n");
+                printf("4) -\n");
+                printf("5) -\n");
+                printf("6) -\n");
+                /*printf("7) -\n");
+                printf("8) -\n");*/
                 break;
+
+            /*case 6:
+                printf("\nSabato:\n\n");
+                printf("1) -\n");
+                printf("2) -\n");
+                printf("3) -\n");
+                printf("4) -\n");
+                printf("5) -\n");
+                printf("6) -\n");
+                printf("7) -\n");
+                printf("8) -\n");
+                break;
+            */
                 
             case 0:
-            	printf("Lunedi':\t1 italiano\t2 storia\t3 matematica\t4 matematica\t5 storia\t6 scienze\n");
-            	printf("Martedi':\t1 fisica\t2 fisica\t3 arte\t\t4 scienze\t5 inglese\t6 filosofia\n");
-            	printf("Mercoledi':\t1 italiano\t2 italiano\t3 informatica\t4 matematica\t5 ginnastica\t6 ginnastica\n");
-            	printf("Giovedi':\t1 scienze\t2 inglese\t3 filosofia\t4 fisica\t5 religione\t6 arte\n");
-            	printf("Venerdi':\t1 informatica\t2 inglese\t3 scienze\t4 scienze\t5 italiano\t6 matematica\n");
+            	printf("Lunedi':\t1 -\t2 -\t3 -\t4 -\t5 -\t6 -\t7 -\t8 -\n");
+            	printf("Martedi':\t1 -\t2 -\t3 -\t4 -\t5 -\t6 -\t7 -\t8 -\n");
+            	printf("Mercoledi':\t1 -\t2 -\t3 -\t4 -\t5 -\t6 -\t7 -\t8 -\n");
+            	printf("Giovedi':\t1 -\t2 -\t3 -\t4 -\t5 -\t6 -\t7 -\t8 -\n");
+            	printf("Venerdi':\t1 -\t2 -\t3 -\t4 -\t5 -\t6 -\t7 -\t8 -\n");
+                /*printf("Sabato:\t1 -\t2 -\t3 -\t4 -\t5 -\t6 -\t7 -\t8 -\n");*/
+                
             	break;
             	
             default:
